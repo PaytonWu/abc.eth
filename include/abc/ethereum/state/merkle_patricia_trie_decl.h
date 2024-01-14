0f5a87e6ac5ad35cc724e533ac53a90cf8140d5d
payton.wu <payton.wu@outlook.com>
2024-01-14 23:18:34 +0800

    🚧 mpt (1)

---
+++ include/abc/ethereum/state/merkle_patricia_trie_decl.h
@@ -0,0 +1,19 @@
+// Copyright(c) 2023 - present, Payton Wu (payton.wu@outlook.com) & the contributors.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#ifndef ABC_ETH_INCLUDE_ABC_ETHEREUM_STATE_MPT_DECL
+#define ABC_ETH_INCLUDE_ABC_ETHEREUM_STATE_MPT_DECL
+
+#pragma once
+
+namespace abc::ethereum::state
+{
+
+class merke_patricia_trie
+{
+
+};
+
+}
+
+#endif //ABC_ETH_INCLUDE_ABC_ETHEREUM_STATE_MPT_DECL
