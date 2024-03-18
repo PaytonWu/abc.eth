95270283d9d327bec562b48bac093dc6f7cbc634
payton.wu <payton.wu@outlook.com>
2024-03-18 23:45:18 +0800

    🚧 MTP: insert........

---
+++ include/abc/ethereum/trie/triedb/hashdb/database_reader_fwd_decl.h
@@ -0,0 +1,17 @@
+// Copyright(c) 2024 - present, Payton Wu (payton.wu@outlook.com) & the contributors.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+
+#if !defined(ABC_ETH_INCLUDE_ABC_ETHEREUM_TRIE_TRIEDB_HASHDB_DATABASE_READER_FWD_DECL)
+#define ABC_ETH_INCLUDE_ABC_ETHEREUM_TRIE_TRIEDB_HASHDB_DATABASE_READER_FWD_DECL
+
+#pragma once
+
+namespace abc::ethereum::trie::triedb::hashdb
+{
+
+template <typename PersistentDBT>
+class [[nodiscard]] database_reader;
+
+}
+
+#endif // ABC_ETH_INCLUDE_ABC_ETHEREUM_TRIE_TRIEDB_HASHDB_DATABASE_READER_FWD_DECL
