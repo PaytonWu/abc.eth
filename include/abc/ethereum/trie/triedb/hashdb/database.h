3e63864b412f56cd155b8d60fe1a1646cc7a995d
payton.wu <payton.wu@outlook.com>
2024-03-19 23:30:24 +0800

    🚧 MTP: insert.........

--- include/abc/ethereum/trie/triedb/hashdb/database.h
+++ include/abc/ethereum/trie/triedb/hashdb/database.h
@@ -12,6 +12,24 @@ namespace abc::ethereum::trie::triedb::hashdb
 {
 
 template <typename DatabaseT>
+database<DatabaseT>::database(std::unique_ptr<DatabaseT> database) : database_{std::move(database)}
+{
+}
+
+template <typename DatabaseT>
+auto
+database<DatabaseT>::insert(const int & hash, bytes_t node) -> void
+{
+    if (dirties_.find(hash) != dirties_.end())
+    {
+        return;
+    }
+    // memcacheDirtyWriteMeter.Mark(int64(len(node)));
+
+    dirties_.emplace(hash, std::move(node));
+}
+
+template <typename DatabaseT>
 auto
 database<DatabaseT>::node(h256_t const & hash) const -> expected<bytes_t, std::error_code>
 {
@@ -44,6 +62,34 @@ database<DatabaseT>::node(h256_t const & hash) const -> expected<bytes_t, std::e
     }
 
     return make_unexpected(std::make_error_code(std::errc::no_such_file_or_directory));
+}
+
+template <typename DatabaseT>
+auto
+database<DatabaseT>::commit(h256_t const & root) -> expected<void, std::error_code>
+{
+    if (root.empty())
+    {
+        return make_unexpected(std::make_error_code(std::errc::invalid_argument));
+    }
+
+    for (auto const & [hash, node] : dirties_)
+    {
+        if (node.empty())
+        {
+            cleans_.erase(hash);
+        }
+        else
+        {
+            cleans_.emplace(hash, node);
+        }
+    }
+
+    dirties_.clear();
+
+    return;
+
+    // return database_->commit(root);
 }
 
 } // namespace abc::ethereum::trie::triedb::hashdb
