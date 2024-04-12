ad387edca347cf183e80c1f4fb81358136b1e1fc
payton.wu <payton.wu@outlook.com>
2024-04-13 00:29:55 +0800

    add an UT for decode_node.

--- include/abc/ethereum/rlp/adaptor/adaptor.h
+++ include/abc/ethereum/rlp/adaptor/adaptor.h
@@ -10,6 +10,7 @@
 #include "abc/bytes_view.h"
 #include "abc/fixed_bytes.h"
 #include "abc/uint128.h"
+#include "cpp/array.h"
 #include "cpp/optional.h"
 #include "cpp/shared_ptr.h"
 #include "unsigned_integral.h"
--- tests/trie/node_decode.cpp
+++ tests/trie/node_decode.cpp
@@ -1,20 +1,20 @@
 // Copyright(c) 2024 - present, Payton Wu (payton.wu@outlook.com) & the contributors.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
-#include <abc/ethereum/trie/node_decode.h>
-#include <abc/ethereum/rlp/simple_buffer.h>
-#include <abc/ethereum/rlp/pack.h>
 #include <abc/bytes_view.h>
+#include <abc/ethereum/rlp/pack.h>
+#include <abc/ethereum/rlp/simple_buffer.h>
+#include <abc/ethereum/trie/node_decode.h>
 
 #include <gtest/gtest.h>
 #include <range/v3/algorithm/fill.hpp>
 
 #include <any>
 #include <array>
+#include <memory>
 #include <string_view>
 #include <utility>
 #include <variant>
-#include <memory>
 
 auto
 new_test_full_node(std::any data) -> std::array<std::any, 17>
@@ -54,7 +54,8 @@ struct full_node_data_1
     abc::bytes_t data16{};
 };
 
-void init_full_node_data_1(full_node_data_1 & fn_data, std::string_view data15, std::string_view data16)
+void
+init_full_node_data_1(full_node_data_1 & fn_data, std::string_view data15, std::string_view data16)
 {
     fn_data.data0.fill(1);
     fn_data.data1.fill(2);
@@ -82,14 +83,51 @@ template <>
 struct as<full_node_data_1>
 {
     auto
-    operator()(rlp::object const & o) const -> full_node_data_1;
+    operator()(rlp::object const & o) const -> full_node_data_1
+    {
+        if (o.type != rlp::type::list)
+        {
+            abc::throw_error(make_error_code(abc::ethereum::rlp::errc::type_error));
+        }
+
+        if (o.data.array.size != 17)
+        {
+            abc::throw_error(make_error_code(abc::ethereum::rlp::errc::type_error));
+        }
+
+        full_node_data_1 v;
+
+        v.data16 = o.data.array.ptr[16].as<abc::bytes_t>();
+        v.data15 = o.data.array.ptr[15].as<std::array<abc::bytes_t, 17>>();
+        v.data14 = o.data.array.ptr[14].as<abc::bytes32_t>();
+        v.data13 = o.data.array.ptr[13].as<abc::bytes32_t>();
+        v.data12 = o.data.array.ptr[12].as<abc::bytes32_t>();
+        v.data11 = o.data.array.ptr[11].as<abc::bytes32_t>();
+        v.data10 = o.data.array.ptr[10].as<abc::bytes32_t>();
+        v.data9 = o.data.array.ptr[9].as<abc::bytes32_t>();
+        v.data8 = o.data.array.ptr[8].as<abc::bytes32_t>();
+        v.data7 = o.data.array.ptr[7].as<abc::bytes32_t>();
+        v.data6 = o.data.array.ptr[6].as<abc::bytes32_t>();
+        v.data5 = o.data.array.ptr[5].as<abc::bytes32_t>();
+        v.data4 = o.data.array.ptr[4].as<abc::bytes32_t>();
+        v.data3 = o.data.array.ptr[3].as<abc::bytes32_t>();
+        v.data2 = o.data.array.ptr[2].as<abc::bytes32_t>();
+        v.data1 = o.data.array.ptr[1].as<abc::bytes32_t>();
+        v.data0 = o.data.array.ptr[0].as<abc::bytes32_t>();
+
+        return v;
+    }
 };
 
 template <>
 struct convert<full_node_data_1>
 {
     auto
-    operator()(rlp::object const & o, full_node_data_1 & v) const -> rlp::object const &;
+    operator()(rlp::object const & o, full_node_data_1 & v) const -> rlp::object const &
+    {
+        v = as<full_node_data_1>{}(o);
+        return o;
+    }
 };
 
 template <>
@@ -116,6 +154,8 @@ struct pack<full_node_data_1>
         o.pack(v.data14);
         o.pack(v.data15);
         o.pack(v.data16);
+
+        return o;
     }
 };
 
@@ -123,10 +163,12 @@ template <>
 struct object<full_node_data_1>
 {
     auto
-    operator()(rlp::object& , full_node_data_1 const & ) const -> void;
+    operator()(rlp::object &, full_node_data_1 const &) const -> void
+    {
+    }
 };
 
-}
+} // namespace abc::ethereum::rlp::adaptor
 
 TEST(node_decode, decode_nested_node)
 {
@@ -150,6 +192,9 @@ TEST(node_decode, decode_nested_node)
     fn_data.data15[16] = abc::bytes_view_t{"subnode"};
 
     abc::ethereum::rlp::simple_buffer buf;
-    abc::ethereum::rlp::packer packer{ buf };
+    abc::ethereum::rlp::packer packer{buf};
+    packer.pack(fn_data);
 
+    auto r = abc::ethereum::trie::decode_node(abc::h256_t::from(abc::bytes_view_t{"0123456789abcdef0123456789abcdef"}).value(), buf.bytes_view());
+    ASSERT_TRUE(r.has_value());
 }
